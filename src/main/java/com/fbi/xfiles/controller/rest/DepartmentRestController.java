19601087d3ee50a84cfd4e572a371f65fcde767f
edersonmonstro <edersonmedeiros@gmail.com>
2024-04-23 10:21:36 -0400

    feature: delete and edit operations added for agents and departments. This Closes #13.

--- src/main/java/com/fbi/xfiles/controller/rest/DepartmentRestController.java
+++ src/main/java/com/fbi/xfiles/controller/rest/DepartmentRestController.java
@@ -2,8 +2,6 @@ package com.fbi.xfiles.controller.rest;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
