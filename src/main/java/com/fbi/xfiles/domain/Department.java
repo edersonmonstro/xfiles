4812e5a7e0bf1e80f4d91a87975fc360213d6fb2
Ederson <edersoncolares@defensoria.am.def.br>
2024-05-14 10:16:57 -0400

    wip: to use records, we need to first update java and spring project version. changed java from 1.8 to 17 and spring from 2.1.6.RELEASE to 3.2.5. We stopped using springfox for swagger (discontinued since 2020) and adopted springdoc. Next step is really explore records.

--- src/main/java/com/fbi/xfiles/domain/Department.java
+++ src/main/java/com/fbi/xfiles/domain/Department.java
@@ -6,15 +6,15 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.PreRemove;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PreRemove;
+import jakarta.persistence.Table;
 
 
 @Entity
--- src/test/java/com/fbi/xfiles/XfilesApplicationTests.java
+++ src/test/java/com/fbi/xfiles/XfilesApplicationTests.java
@@ -2,7 +2,7 @@ package com.fbi.xfiles;
 
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
