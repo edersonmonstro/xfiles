7656fa6eeb41c6d3e70481ec69cf3dedcbab24b4
edersonmonstro <edersonmedeiros@gmail.com>
2024-12-06 09:43:17 -0400

    refactor: populate departments table at system start. Added department acronyms

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- Inserting departments
+INSERT INTO department (id, name, acronym, creation_date, email, active) VALUES
+('11111111-1111-1111-1111-111111111111', 'Criminal Investigation', 'CIN', '2024-01-01', 'cid@fbi.org', true),
+('22222222-2222-2222-2222-222222222222', 'Narcotics', 'NAR','2024-01-02', 'nd@fbi.org', true),
+('33333333-3333-3333-3333-333333333333', 'Police Intelligence', 'INT','2024-01-03', 'pid@fbi.org', true),
+('44444444-4444-4444-4444-444444444444', 'Homicide and Person Protection', 'HOM','2024-01-04', 'hppd@fbi.org', true),
+('55555555-5555-5555-5555-555555555555', 'Cyber Crime Suppression', 'CYB','2024-01-05', 'ccsd@fbi.org', true),
+('66666666-6666-6666-6666-666666666666', 'Internal Affairs', 'IAF','2024-01-06', 'iad@fbi.org', true),
+('77777777-7777-7777-7777-777777777777', 'Special Operations', 'SPO','2024-01-07', 'sod@fbi.org', true),
+('88888888-8888-8888-8888-888888888888', 'Forensic Technical', 'FOR','2024-01-08', 'ftd@fbi.org', true),
+('99999999-9999-9999-9999-999999999999', 'Administrative Police', 'ADM','2024-01-09', 'apd@fbi.org', true),
+('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'Organized Crime Suppression', 'ORG','2024-01-10', 'ocsd@fbi.org', true),
+('00000000-0000-0000-0000-000000000000', 'X-Files Division', 'XFL','2024-01-09', 'xfiles@fbi.org', true);
